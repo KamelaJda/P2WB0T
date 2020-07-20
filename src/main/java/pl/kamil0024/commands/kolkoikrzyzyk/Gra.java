cb0a6cb6526ee6be0c65befdb70dd45fe4a9a868
KAMIL0024 <kamilbydpol@gmail.com>
2020-07-21 01:53:14 +0200

    Nowe rzeczy

--- src/main/java/pl/kamil0024/commands/kolkoikrzyzyk/Gra.java
+++ src/main/java/pl/kamil0024/commands/kolkoikrzyzyk/Gra.java
@@ -7,13 +7,14 @@ import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
-import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import pl.kamil0024.bdate.BDate;
-import pl.kamil0024.core.logger.Log;
+import pl.kamil0024.commands.kolkoikrzyzyk.entites.Slot;
 import pl.kamil0024.core.util.BetterStringBuilder;
 import pl.kamil0024.core.util.EventWaiter;
 
 import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @Data
@@ -21,18 +22,18 @@ public class Gra {
 
     public static String KOLKO = "\uD83D\uDD34";
     public static String KRZYZYK = "❌";
+    public static String PUSTE = "\uD83D\uDED1";
 
     private Member osoba1;
     private Member osoba2;
     private Member kogoRuch;
-
     private TextChannel channel;
+    private Message botMsg;
 
     private long dataRozpoczecia;
 
-    private Message botMsg;
-
     private EventWaiter eventWaiter;
+    private Slot slot;
 
     public Gra(Member osoba1, Member osoba2, TextChannel channel, EventWaiter eventWaiter) {
         this.osoba1 = osoba1;
@@ -43,6 +44,7 @@ public class Gra {
         this.dataRozpoczecia = new BDate().getTimestamp();
 
         this.eventWaiter = eventWaiter;
+        this.slot = new Slot();
     }
 
     public void create() {
@@ -61,11 +63,13 @@ public class Gra {
         EmbedBuilder eb = new EmbedBuilder();
         eb.setColor(Color.cyan);
         eb.setTitle("Gra w Kółko i Krzyżyk");
-        eb.setDescription(getOsoba1().getAsMention() + " vs " + getOsoba2().getAsMention());
+        eb.setDescription(getOsoba1().getAsMention() + " vs " + getOsoba2().getAsMention()
+                + "\n\n" + getOsoba1().getAsMention() + " " + getEmote(getOsoba1())
+                + "\n" + getOsoba2().getAsMention() + " " + getEmote(getOsoba2()));
         eb.addField("Kogo ruch?", getKogoRuch().getAsMention(), false);
 
         eb.addField("Plansza", getPlansza(), false);
-        eb.addField("Tipy", "Aby się ruszyć trzeba napisać `gra: <nr. planszy>`. np. `gra: 1b`", false);
+        eb.addField("Tipy", "Aby się ruszyć trzeba napisać `gra: <nr. planszy>`. np. `gra: 1b`\nJeżeli gracz nie ruszy się przez **30 sekund** gra zostaje przerwana!", false);
 
         return eb;
     }
@@ -74,13 +78,15 @@ public class Gra {
         BetterStringBuilder sb = new BetterStringBuilder();
         sb.append("```");
 
-        sb.appendLine("1 ❌ | ❌ | ❌");
-        sb.appendLine("2 ❌ | ❌ | ❌");
-        sb.appendLine("3 ❌ | ❌ | ❌");
+        sb.appendLine("1 %s | %s | %s");
+        sb.appendLine("2 %s | %s | %s");
+        sb.appendLine("3 %s | %s | %s");
         sb.appendLine("   A    B    C");
 
         sb.append("```");
-        return sb.toString();
+
+        HashMap<Integer, String> s = getSlot().getSloty();
+        return String.format(sb.toString(), s.get(1), s.get(2), s.get(3), s.get(4), s.get(5), s.get(6), s.get(7), s.get(8), s.get(9));
     }
 
     private boolean checkRuch(GuildMessageReceivedEvent event) {
@@ -88,11 +94,24 @@ public class Gra {
         if (msg.isEmpty() || !msg.toLowerCase().startsWith("gra:")
                 || !getKogoRuch().getId().equals(event.getAuthor().getId())
                 || !getChannel().getId().equals(event.getChannel().getId())) return false;
+        event.getMessage().delete().queue();
+        msg = msg.replaceAll(" ", "");
+
+        String[] plansza = msg.split("");
+        if (plansza.length != 2) {
+            event.getChannel().sendMessage(event.getAuthor().getAsMention() + ", zły numer planszy! Użycie: `gra: <nr. planszy>`. np. `gra: 1b`")
+                    .queueAfter(6, TimeUnit.SECONDS, m -> m.delete().queue());
+            return false;
+        }
+        if (!getSlot().check(msg,this, event.getMember())) {
+            event.getChannel().sendMessage(event.getAuthor().getAsMention() + ", zły format planszy (lub chcesz zając zajęte już pole)! Użycie: `gra: <nr. planszy>`. np. `gra: 1b`")
+                    .queueAfter(6, TimeUnit.SECONDS, m -> m.delete().queue());
+            return false;
+        }
         return true;
     }
 
     private void ruch(GuildMessageReceivedEvent event) {
-        String msg = event.getMessage().getContentRaw();
         setKogoRuch(getKogoRuch().getId().equals(osoba1.getId()) ? osoba2 : osoba1);
         getBotMsg().editMessage(getEmbed().build()).complete();
         waitForRuch();
@@ -100,11 +119,15 @@ public class Gra {
 
     public void waitForRuch() {
         eventWaiter.waitForEvent(GuildMessageReceivedEvent.class,
-                this::checkRuch, this::ruch, 1, TimeUnit.MINUTES, this::stopGame);
+                this::checkRuch, this::ruch, 30, TimeUnit.SECONDS, this::stopGame);
     }
 
     private void stopGame() {
         getBotMsg().editMessage("koniec").complete();
+    }
+
+    public String getEmote(Member member) {
+        return member.getId().equals(getOsoba1().getId()) ? KOLKO : KRZYZYK;
     }
 
 }
---
+++ src/main/java/pl/kamil0024/commands/kolkoikrzyzyk/entites/Slot.java
@@ -0,0 +1,71 @@
+package pl.kamil0024.commands.kolkoikrzyzyk.entites;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import net.dv8tion.jda.api.entities.Member;
+import pl.kamil0024.commands.kolkoikrzyzyk.Gra;
+
+import java.util.HashMap;
+
+
+public class Slot {
+
+    @Getter public HashMap<Integer, String> sloty;
+
+    public Slot() {
+        this.sloty = new HashMap<>();
+
+        for (int i = 1; i < 10; i++) {
+            sloty.put(i, Gra.PUSTE);
+        }
+
+    }
+
+    public boolean check(String s, Gra gra, Member osoba) {
+        String[] slot = s.split("");
+
+        int jeden;
+        String tak = slot[1].toLowerCase();
+
+        try {
+            jeden = Integer.parseInt(slot[0]);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        if (jeden < 1 || jeden > 3) return false;
+        if (!tak.equals("a") && !tak.equals("b") && !tak.equals("c")) return false;
+
+        String kekw = sloty.get(format(slot));
+        if (!kekw.equals(Gra.PUSTE)) return false;
+
+        sloty.put(format(slot), gra.getEmote(osoba));
+        return true;
+    }
+
+    private int format(String[] s) {
+        // s[0] = numer to lewej
+        // s[1] = litera na dole
+
+        // 1 [1] | [2] | [3]
+        // 2 [4] | [5] | [6]
+        // 3 [7] | [8] | [9]
+        //    A     B    C
+
+        int slot = Integer.parseInt(s[0]);
+
+        if (s[1].equals("a")) return jebacMatme(slot, 1, 4, 7);
+        if (s[1].equals("b")) return jebacMatme(slot, 3, 5, 8);
+        if (s[1].equals("c")) return jebacMatme(slot, 4, 6, 9);
+        return slot + 3;
+    }
+
+    private int jebacMatme(int slot, int moze1, int moze2, int moze3) {
+        if (slot == moze1) return moze1;
+        if (slot == moze2) return moze2;
+        if (slot == moze3) return moze3;
+        throw new UnsupportedOperationException();
+    }
+
+}
