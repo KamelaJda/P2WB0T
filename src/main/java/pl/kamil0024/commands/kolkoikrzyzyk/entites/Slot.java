45bc629057569933be384deeccb4b81a7b6f7304
KAMIL0024 <kamilbydpol@gmail.com>
2020-07-21 19:59:32 +0200

    Nowe rzeczy

--- src/main/java/pl/kamil0024/commands/kolkoikrzyzyk/entites/Slot.java
+++ src/main/java/pl/kamil0024/commands/kolkoikrzyzyk/entites/Slot.java
@@ -7,6 +7,7 @@ import net.dv8tion.jda.api.entities.Member;
 import pl.kamil0024.commands.kolkoikrzyzyk.Gra;
 import pl.kamil0024.core.logger.Log;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 
@@ -68,20 +69,24 @@ public class Slot {
         // 3 [7] | [8] | [9]
         //    A     B    C
 
+        Log.debug(Arrays.toString(s));
         int slot = Integer.parseInt(s[0]);
 
         if (s[1].equals("a")) return jebacMatme(slot, 1, 4, 7);
         if (s[1].equals("b")) return jebacMatme(slot, 2, 5, 8);
         if (s[1].equals("c")) return jebacMatme(slot, 3, 6, 9);
-        return slot + 3;
+        Log.debug("kurwa " + s[1]);
+        return 0;
     }
 
     private int jebacMatme(int slot, int moze1, int moze2, int moze3) {
         if (slot == moze1) return moze1;
         if (slot == moze2) return moze2;
         if (slot == moze3) return moze3;
+        Log.debug("xdddddddddddddddddddd");
         Log.debug(slot + "");
         Log.debug(moze1 + moze2 + moze3 + "");
+        Log.debug("xdddddddddddddddddddd");
         throw new UnsupportedOperationException();
     }
 
